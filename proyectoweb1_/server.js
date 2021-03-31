21f9bbc7a52b5812b98fab2bcd02e5538c003373
root <root@ip-172-31-9-150.ec2.internal>
2021-03-31 01:57:37 +0000

    subir

--- proyectoweb1_/server.js
+++ proyectoweb1_/server.js
@@ -1,11 +1,25 @@
 const express = require("express") 
 var app = express()
 var data
+var lat
+var lon
+var tim
+var path = require('path')
+
 // fs es para manipular archivos en general (en este caso un txt)
 const fs = require("fs")
+
 app.listen(37778);
 
-app.use(express.static("estatico"))  
+app.use(express.static(__dirname));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname + '/estatico/index.html'));
+});
+
+app.get('/ubicartaxi', (request, response) => {
+  response.sendFile(path.join(__dirname + '/estatico/ubicar.html'));
+});
 
 // conexión con la base de datos
 const {pool,Client}= require("pg")
@@ -19,8 +33,10 @@ client.connect()
 
 // sniffer udp 
 const dgram = require('dgram');
+
 //ejecuto el metodo create socket para que me devuelva un objeto en la variable datos
 const datos = dgram.createSocket('udp4');
+
 //cacho error del socket
 datos.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
@@ -30,25 +46,23 @@ datos.on('error', (err) => {
 //obtengo de datos el mensaje que envio desde la app
 //SINFFER
 datos.on('message', (msg, rinfo) =>  {
-//UTF8 es un estandar para leer los datos que me llegan
     msg = msg.toString()
     fs.writeFile('/var/www/taxis/TAXIS-web-server-2/proyectoweb1_/estatico/result.txt', msg, err => {
               if (err) throw err;
     })
     console.log(msg)
     data = msg.split("/")
-    var lat = ("\'"+data[0]+"\'")
-    var lon = ("\'"+data[1]+"\'")
-    var tim = ("\'"+data[2]+"\'")
+    lat = ("\'"+data[0]+"\'")
+    lon = ("\'"+data[1]+"\'")
+    tim = ("\'"+data[2]+"\'")
     console.log(lat)
-
-
-    client.query('INSERT INTO public.geodatos("latitud","longitud","time")VALUES ('+lat+','+lon+','+tim+');', (err,res)=>{
-       console.log(err,res);
-
-   })
-
   });
 
-  // Fijación del puerto UDP 
-  datos.bind(37777);
+setInterval(function() {
+    client.query('INSERT INTO public.geodatos("latitud","longitud","time")VALUES ('+lat+','+lon+','+tim+');', (err,res)=>{
+       console.log(err,res);
+    })
+},5000)
+
+// Fijación del puerto UDP 
+datos.bind(37777);
