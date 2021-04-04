4b4bac56ff8137eaeb924910bfb01c6f1c1cdf44
root <root@ip-172-31-9-150.ec2.internal>
2021-04-04 21:57:45 +0000

    mejoras del calendario

--- proyectoweb1_/estatico/calendar.js
+++ proyectoweb1_/estatico/calendar.js
@@ -4,20 +4,49 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom:18, minZoom:9,
 }).addTo(map);
+var fecha1
+var fecha2
+var hora1
+var hora2
+var f1
+var f2
 
 function obtenerdatos(){
 
+//	var cal1 = null
+//	var cal2 = null
+//	console.log(cal1)
+
 	var cal1 = document.getElementById("calendario1").value;
 	var cal2 = document.getElementById("calendario2").value;
-	console.log(cal1)
+//	console.log(cal1)
 
 	var split = cal1.split("T")
 	var split2 = cal2.split("T")
 
 	fecha1 = split[0]
+	fecha2 = split2[0]
 	hora1 = split[1]
-	fecha2 = split[0]
-	hora2 = split[1]
+	hora2 = split2[1]
+	// # de miliseg desde 00:00:00 abril 1970
+	f1 = Date.parse(fecha1+","+hora1)
+	f2 = Date.parse(fecha2+","+hora2)
+	//
+
+	console.log(f1+"/"+f2)
+
+	// verificar:
+	// 1. ¿el usuario realmente digitó todos los datos necesarios?
+
+	if (cal1 == [] || cal2 == []) {
+	alert("Ups! verifica tus datos")
+	}
+
+	// 2. ¿la fecha/hora final realmente ocurre después que la fecha/hora inicial?
+	if (f1>f2) {
+		alert("Ups! la fecha/hora inicial debe ocurrir antes que la final")
+	}
+
 
 //	return [fecha1,hora1,fecha2,hora2]
 
